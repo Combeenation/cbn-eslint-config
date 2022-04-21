98722ac6cae78a119e6ce9976a70495ba030ee9f
HAR <michael.harrer@combeenation.com>
2022-04-21 09:00:44 +0200

    Adjust settings for new coding conventions
    #CB-6659

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ module.exports = {
     'no-unused-vars': ['off'],
     // Use typescript version of "no-unused-vars" rule, because the default rule doesn't recognize certain typescript
     // features like enumerations
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-    'no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_', args: 'none' }],
+    'no-use-before-define': ['warn'],
+
+    // Prettier takes care of max length in code and eslint warns about prettier warnings due to the rule
+    // 'prettier/prettier'. Setting max-len to 120 for code here would add some linter warnings in code where prettier
+    // does not obey to the 120 char line length, so we're "supressing" it with 1000...
+    'max-len': ['warn', { code: 1000, comments: 120 }],
 
     // Misc
     'no-empty': ['warn', { allowEmptyCatch: true }],
@@ -28,5 +33,6 @@ module.exports = {
     // TS specific things
     '@typescript-eslint/ban-ts-comment': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
   },
 };
